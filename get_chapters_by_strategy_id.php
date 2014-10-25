4249ff6da2546b70db5a7612187aaf82b36ccc3f
wytrivail <wytrivail@163.com>
2014-10-25 16:32:19 +0800

    modify

--- get_chapters_by_strategy_id.php
+++ get_chapters_by_strategy_id.php
@@ -2,18 +2,18 @@
 include_once("mysql_tool.php");
 class chapter_item{
     public $name;
-    public $system_id;
+    public $id;
     function __construct($id,$name){
-        $this->system_id = $id;
+        $this->id = $id;
         $this->name = $name;
     }
 }
 $start = $_GET['start'];
 $length = $_GET['length'];
-$result = db_query("select * from chapter where strategy_id = '$_GET[strategy_id]' order by section limit $start,$length");
+$result = db_query("select * from chapter where strategy_id = '$_GET[id]' order by section limit $start,$length");
 $data = array();
 while($row=mysql_fetch_array($result)){
     array_push($data, new chapter_item($row['id'],urlencode($row['chapter_name'])));
 }
-echo urldecode(json_encode(array($data)));
+echo urldecode(json_encode($data));
 ?>
